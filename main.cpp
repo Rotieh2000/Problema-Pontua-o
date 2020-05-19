9ca7b9add02d68225442d1df943ee72980b5289d
Rotieh2000 <heiztorhssilva@gmail.com>
2020-05-19 08:18:20 -0300

    Leitura volume

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main() {
-  double compras, atraso, ticket, pontos, pagamento, scorev,scorei, scorefp;
+  float compras, atraso, ticket, pontos, pagamento, scorev,scorei, scorefp;
  cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
  cout << "----------------------------" << endl;
  cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
  cout << " " << endl;
+ cout << "Quantas compras o cliente fez no último ano? ";
+ cin >> compras;
+ cout << "Qual o ticket médio? ";
+ cin >> ticket;
  cout << "Quantas vezes o cliente atrasou o pagamento? "; 
  cout << "A maioria das compras foi em dinheiro, cartão ou boleto (D/B/C)?";
- cout << " " << endl;
+ cout << "  " << endl;
  cout << "Score do volume de compras = " << endl;
  cout << "" << endl;
  cout << "Score de inadiplência = " << endl;
