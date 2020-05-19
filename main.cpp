fc9593976136368561fd1493a91116908f996da2
Rotieh2000 <heiztorhssilva@gmail.com>
2020-05-19 08:54:55 -0300

    Score de volume de compras

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
-  float compras, atraso, ticket, pontos, pagamento, scorev,scorei, scorefp, classfinal;
+  float compras, atraso, ticket, pontosv, pontosi, pontosfp, pagamento, scorev,scorei, scorefp, classfinal;
   scorev = 0;
   scorei = 0;
   scorefp = 0;
-  pontos = 0;
+  pontosi = 0;
+  pontosfp = 0;
   compras = 0;
   atraso = 0;
   ticket= 0;
@@ -20,11 +21,23 @@ int main() {
  cin >> compras;
  cout << "Qual o ticket médio? ";
  cin >> ticket;
- cout << "Quantas vezes o cliente atrasou o pagamento? "; 
+  if(compras == 0){
+   pontosv = 0;
+   }
+   if((compras == 2) && (ticket <= 3000)){
+     pontosv = 20;
+   }
+   if((compras >= 2) && (ticket <= 3000)){
+     pontosv = 40;
+   }
+   if(ticket > 3000){
+     pontosv = 60;
+   }
+ cout << "Quantas vezes o cliente atrasou o pagamento? ";
  cout << "A maioria das compras foi em dinheiro, cartão ou boleto (D/B/C)? ";
  cin >> pagamento;
  cout << "  " << endl;
- cout << "Score do volume de compras = " << scorev <<endl;
+ cout << "Score do volume de compras = " << pontosv <<endl;
  cout << " " << endl;
  cout << "Score de inadiplência = " << scorei << endl;
  cout << "Score de forma de pagamento = " << scorefp << endl;
