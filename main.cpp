cf9ba4772d444c558605e8b98e1a0a825fd71dee
Rotieh2000 <heiztorhssilva@gmail.com>
2020-05-19 08:05:12 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-#include <bits/stc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-  
+  double compras, atraso, ticket, pontos, pagamento, scorev,scorei, scorefp;
+ cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+ cout << "----------------------------" << endl;
+ cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
+ cout << " " << endl;
+ cout << "Quantas vezes o cliente atrasou o pagamento? "; 
+ cout << "A maioria das compras foi em dinheiro, cartão ou boleto (D/B/C)?";
+ cout << " " << endl;
+ cout << "Score do volume de compras = " << endl;
+ cout << "" << endl;
+ cout << "Score de inadiplência = " << endl;
+ cout << "Score de forma de pagamento" << endl;
+ cout << " " << endl;
+ cout << "Classificação final = " << endl;
+
 }
