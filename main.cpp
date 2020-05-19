8fc8ebe6da9880af750f5e972477b020f12f82b6
Rotieh2000 <heiztorhssilva@gmail.com>
2020-05-19 08:28:34 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
-  float compras, atraso, ticket, pontos, pagamento, scorev,scorei, scorefp;
+  float compras, atraso, ticket, pontos, pagamento, scorev,scorei, scorefp, classfinal;
+  scorev = 0;
+  scorei = 0;
+  scorefp = 0;
+  pontos = 0;
+  compras = 0;
+  atraso = 0;
+  ticket= 0;
+  pagamento = 0;
  cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
  cout << "----------------------------" << endl;
  cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
@@ -13,13 +21,14 @@ int main() {
  cout << "Qual o ticket médio? ";
  cin >> ticket;
  cout << "Quantas vezes o cliente atrasou o pagamento? "; 
- cout << "A maioria das compras foi em dinheiro, cartão ou boleto (D/B/C)?";
+ cout << "A maioria das compras foi em dinheiro, cartão ou boleto (D/B/C)? ";
+ cin >> pagamento;
  cout << "  " << endl;
- cout << "Score do volume de compras = " << endl;
- cout << "" << endl;
- cout << "Score de inadiplência = " << endl;
- cout << "Score de forma de pagamento" << endl;
+ cout << "Score do volume de compras = " << scorev <<endl;
  cout << " " << endl;
- cout << "Classificação final = " << endl;
+ cout << "Score de inadiplência = " << scorei << endl;
+ cout << "Score de forma de pagamento = " << scorefp << endl;
+ cout << " " << endl;
+ cout << "Classificação final = " << classfinal << endl;
 
 }
